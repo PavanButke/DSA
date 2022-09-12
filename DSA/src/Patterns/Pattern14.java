b4ca077cb206f3b318535b76c4962ea68573ed9e
PavanButke <butkepavan55@gmail.com>
2022-09-12 22:50:50 +0530

    Pattern 14

---
+++ DSA/src/Patterns/Pattern14.java
@@ -0,0 +1,21 @@
+package Patterns;
+import java.util.*;
+
+public class Pattern14 {
+
+
+
+	    public static void main(String[] args) {
+	        Scanner scn = new Scanner(System.in);
+	        
+	        int x= scn.nextInt();
+	        
+	        for(int i=1 ; i<= 10 ; i++){
+	            int v= x * i;
+	            
+	            System.out.println(x+" "+"*"+i+" "+"="+" "+v);
+	        }
+
+	    }
+	}
+
