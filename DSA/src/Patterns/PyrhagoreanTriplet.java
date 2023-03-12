039ecea365a6150a9446078cfae424057f8520d3
PavanButke <butkepavan55@gmail.com>
2023-03-13 00:31:43 +0530

    Pythagorean Triplet added

---
+++ DSA/src/Patterns/PyrhagoreanTriplet.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+public class PyrhagoreanTriplet {
+    
+  
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n1= scan.nextInt();
+    int n2= scan.nextInt();
+    int n3= scan.nextInt();
+    
+    boolean isPT= false;
+    if(n1*n1 == (n2*n2 + n3*n3))
+    {
+        isPT= true;
+   }
+   else if(n2*n2 == (n1*n1 + n3*n3))
+   {
+       isPT = true;
+   }
+   else if(n3*n3 == (n2*n2 + n1*n1))
+   {
+       isPT = true;
+   }
+   System.out.println(isPT);
+  }
+  
+}
